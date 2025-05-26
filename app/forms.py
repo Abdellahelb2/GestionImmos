6c6c3e5da5d818bd1f745fee09ee48abb47dec2e
Abdellahelb2 <134738779+Abdellahelb2@users.noreply.github.com>
2025-05-26 20:16:18 +0100

    p

--- app/forms.py
+++ app/forms.py
@@ -96,7 +96,6 @@ class CustomUserChangeForm(forms.ModelForm):
             'first_name',
             'last_name',
             'email',
-            'status',
             'phone_number',
             'profile_picture',
             'date_of_birth',
